bac311ff1026e79eb95e445fb645ec6daab2e4ef
Jose Nogueira <jose.nogueira@eyenov.com>
2021-06-14 17:04:01 +0100

    LLS-3161 - Update react native iOS version
    - Change LiveryReactNativeView to work with the iOS sdk

--- ios/LiveryReactNativeViewManager.swift
+++ ios/LiveryReactNativeViewManager.swift
@@ -1,32 +1,58 @@
+import Livery
+
 @objc(LiveryReactNativeViewManager)
 class LiveryReactNativeViewManager: RCTViewManager {
-
-  override func view() -> (LiveryReactNativeView) {
-    return LiveryReactNativeView()
-  }
+    
+    override func view() -> (LiveryReactNativeView) {
+        return LiveryReactNativeView()
+    }
 }
 
 class LiveryReactNativeView : UIView {
-
-  @objc var color: String = "" {
-    didSet {
-      self.backgroundColor = hexStringToUIColor(hexColor: color)
+    
+    private var isInitialized: Bool = false
+    private var livery: LiverySDK = LiverySDK()
+    private var player: Player?
+        
+    @objc var streamId: String? {
+        didSet {
+            guard let streamId = self.streamId else { return }
+            initializeSDK(streamId: streamId)
+        }
     }
-  }
-
-  func hexStringToUIColor(hexColor: String) -> UIColor {
-    let stringScanner = Scanner(string: hexColor)
-
-    if(hexColor.hasPrefix("#")) {
-      stringScanner.scanLocation = 1
+    
+    
+    private func initializeSDK(streamId: String) {
+        player?.stop()
+        
+        livery.initialize(streamId: streamId) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success:
+                self.isInitialized = true
+                print("Livery SDK initialization was successful")
+                DispatchQueue.main.async {
+                    self.createPlayer()
+                }
+                
+            case .failure(let error):
+                self.isInitialized = false
+                print("Livery SDK initialization failed: \(error.localizedDescription)")
+            }
+        }
     }
-    var color: UInt32 = 0
-    stringScanner.scanHexInt32(&color)
-
-    let r = CGFloat(Int(color >> 16) & 0x000000FF)
-    let g = CGFloat(Int(color >> 8) & 0x000000FF)
-    let b = CGFloat(Int(color) & 0x000000FF)
-
-    return UIColor(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: 1)
-  }
+    
+    private func createPlayer() {
+        // Create the Livery player. This may block for a while.
+        guard let player = livery.createPlayer(options: playerOptions()) else {
+            print("Could not create Livery player instance")
+            return
+        }
+        
+        DispatchQueue.main.async {
+            player.setView(view: self)
+            self.player = player
+        }
+    }
 }
