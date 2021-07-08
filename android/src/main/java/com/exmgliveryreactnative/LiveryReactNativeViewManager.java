3cd788045aaff5ded053d3903ee95ca5aa3386e8
Jose Nogueira <jose.nogueira@eyenov.com>
2021-07-08 17:15:09 +0100

    - Re-structure 'LiveryReactNativeViewManager' to use 'LiveryPlayer'

--- android/src/main/java/com/exmgliveryreactnative/LiveryReactNativeViewManager.java
+++ android/src/main/java/com/exmgliveryreactnative/LiveryReactNativeViewManager.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 
 import android.util.Log;
 
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
@@ -23,7 +24,10 @@ import tv.exmg.livery.LiveryResizeMode;
 import tv.exmg.livery.LiverySDK;
 
 public class LiveryReactNativeViewManager extends SimpleViewManager<View> {
+
     public static final String REACT_CLASS = "LiveryReactNativeView";
+
+    private LiveryPlayer player;
 
     @Override
     @NonNull
@@ -37,24 +41,56 @@ public class LiveryReactNativeViewManager extends SimpleViewManager<View> {
         Log.d("CONSOLE LOG: ","create view insteance");
         final LiveryPlayerView view = new LiveryPlayerView(reactContext.getCurrentActivity());
 
-
         view.setKeepScreenOn(true);
         view.setTargetLatency(3000);
         view.setDebugModeEnabled(true);
 
+        player = new LiveryPlayer(view);
 
+        // Forward lifecycle events to the player.
+        connectPlayerLifecycle(view, reactContext);
 
         return view;
     }
 
     @ReactProp(name = "streamId")
     public void setStreamId(LiveryPlayerView view, String streamId) {
-        LiverySDK.getInstance().initialize(streamId, new LiverySDK.StateListener() {
-           @Override
-           public void stateChanged(LiverySDK.State state) {
-            Log.d("CONSOLE LOG: ","STATE CHANGED");
-            view.createPlayer();
-           }
-        });
+      player.setStreamId(streamId);
     }
+
+  /**
+   * Connect a player to the React Native lifecycle.
+   */
+  private static void connectPlayerLifecycle(final LiveryPlayerView view, final ThemedReactContext context) {
+    // Forward lifecycle events to the view.
+    final LifecycleEventListener lifecycleEventListener = new LifecycleEventListener() {
+      @Override
+      public void onHostResume() {
+        view.onResume();
+      }
+
+      @Override
+      public void onHostPause() {
+        view.onPause();
+      }
+
+      @Override
+      public void onHostDestroy() {
+        view.onDestroy();
+      }
+    };
+
+    // Install the lifecycle listener, and make sure we properly clean up the player.
+    view.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+      @Override
+      public void onViewAttachedToWindow(View v) {
+        context.addLifecycleEventListener(lifecycleEventListener);
+      }
+
+      @Override
+      public void onViewDetachedFromWindow(View v) {
+        context.removeLifecycleEventListener(lifecycleEventListener);
+      }
+    });
+  }
 }
