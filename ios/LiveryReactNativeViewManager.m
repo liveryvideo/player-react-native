77ee91bc8742c8f11f9ec6313428fc9949b5090a
rui.rodrigues <rui.rodrigues@eyenov.com>
2022-01-17 08:33:33 +0000

    LLS-3674 LiverySDK for ReactNative
    - add playbackControlState and interactiveURL view properties
    - change LiveryReactNativeViewManager to use its own LiverySDK and Player to avoid using static variables

--- ios/LiveryReactNativeViewManager.m
+++ ios/LiveryReactNativeViewManager.m
@@ -3,6 +3,8 @@
 @interface RCT_EXTERN_MODULE(LiveryReactNativeViewManager, RCTViewManager)
 
 RCT_EXPORT_VIEW_PROPERTY(streamId, NSString)
+RCT_EXPORT_VIEW_PROPERTY(playbackControlState, NSInteger)
+RCT_EXPORT_VIEW_PROPERTY(interactiveURL, NSString)
 
 RCT_EXPORT_VIEW_PROPERTY(onPlaybackStateDidChange, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onActiveQualityDidChange, RCTBubblingEventBlock)
